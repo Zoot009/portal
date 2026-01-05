e0000057fb8c280156d32fd349f49a13a315c79f
Zoot009 <zootdigitalseo@gmail.com>
2026-01-05 17:53:25 +0530

    changed the font

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${geistSans.variable} ${geistMono.variable} font-bold antialiased`}
         >
           <ThemeProvider
             attribute="class"
